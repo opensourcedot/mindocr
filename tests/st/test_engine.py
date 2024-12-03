d51210a2b321d27af6b1caccb134e256fc599626
ZJL <zhangjunlongtech@foxmail.com>
2024-12-03 11:02:12 +0800

    add test

---
+++ tests/st/test_engine.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+import mindocr
+from mindocr import DetModel
+from mindocr import RecModel
+
+cur_dir = Path(__file__).resolve().parent
+test_file_dir = cur_dir / "test_engine_cfg"
+
+#This specifies the address of the data set used by the test case, which runs the test directly
+CFG_FILE = str(test_file_dir / "crnn_resnet34.yaml")
+IMG_PATHS = str(test_file_dir / "CRNN.png")
+
+
+def test_detmodel_default_cfg():
+    casemodel = DetModel(algo="DB++")
+    res = casemodel.infer(IMG_PATHS)
+
+
+def test_recmodel_default_cfg():
+    casemodel = RecModel(algo="RARE")
+    res = casemodel.infer([IMG_PATHS])
+
+
+def test_recmodel_get_model():
+    casemodel = RecModel(algo="RARE")
+    getmodel = casemodel.get_model()
+
+
+def test_infer_from_yaml():
+    casemodel = RecModel(init_with_config_file=True,config_file_path=CFG_FILE)
+    res = casemodel.infer([IMG_PATHS])
+
+
+if __name__=="__main__":
+    test_detmodel_default_cfg()
+    test_recmodel_default_cfg()
+    test_recmodel_get_model()
+    test_infer_from_yaml()
