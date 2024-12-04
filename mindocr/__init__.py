04ff71bc2c6f18e889e0918a932500094249be22
ZJL <zhangjunlongtech@foxmail.com>
2024-12-04 09:34:42 +0800

    add modelengine module

--- mindocr/__init__.py
+++ mindocr/__init__.py
@@ -1,5 +1,6 @@
-from . import data, losses, metrics, models, postprocess, utils
+from . import data, engine, losses, metrics, models, postprocess, utils
 from .data import *
+from .engine import *
 from .losses import *
 from .metrics import *
 from .models import *
@@ -9,6 +10,7 @@ from .version import __version__
 
 __all__ = []
 __all__.extend(data.__all__)
+__all__.extend(engine.__all__)
 __all__.extend(losses.__all__)
 __all__.extend(models.__all__)
 __all__.extend(postprocess.__all__)
