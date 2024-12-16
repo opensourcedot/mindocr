76a0e2905c6ea0544ebe1c5b2df8576d0090f868
wxsicey <1790571317@qq.com>
2024-12-16 09:25:06 +0800

    add tests

---
+++ deploy/orc_serving/test/serving_client.py
@@ -0,0 +1,22 @@
+"""Client for lenet"""
+import argparse
+import os
+
+
+def read_images():
+    """Read images for directory test_image"""
+    images_buffer = []
+    image_files = []
+    for path, _, file_list in os.walk("mytest/"):
+        for file_name in file_list[:5]:
+            image_file = os.path.join(path, file_name)
+            image_files.append(image_file)
+    for image_file in image_files:
+        with open(image_file, "rb") as fp:
+            images_buffer.append(fp.read())
+    return images_buffer, image_files
+
+
+def run_restful_classify_top1(model_name, restful_address):
+    """RESTful Client for servable lenet and method classify_top1"""
+    print("run_restful_classify_top1-----------")
